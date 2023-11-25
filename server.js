2a10dba64355b1c86b1daf023e61c4f75e1adb6a
Mehul <mehulbohra11@gmail.com>
2023-11-25 17:08:30 +0530

    add dotenv

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import ytdl from "ytdl-core";
 import express from "express";
 import axios from "axios";
 import cors from "cors"
-import cheerio from "cheerio";
+import cheerio from "cheerio"
 import fs from "fs";
+import 'dotenv/config'
 
 const app = express();
+const port = process.env.PORT
+console.log(port)
 
 app.use(cors())
 
@@ -13,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/send/audio", (req, res) => {
     console.log("Server hit");
-    const url = req.body.url;
+    const url = req.body.url.toString();
     console.log(url);
     if (url) {
         
@@ -28,8 +31,8 @@ app.post("/api/send/audio", (req, res) => {
             console.log(title, typeof title);
             res.setHeader("Content-Type", "audio/mpeg");
             res.setHeader(`Content-Disposition`, `attachment; filename=${title}.mp3`);
-            const audio = ytdl(url, { quality: 'highestaudio' });
-            // await audio.pipe(fs.createWriteStream(`audio.mp3`))
+            const audio = ytdl(url,{quality:"highestaudio"});
+            audio.pipe(fs.createWriteStream(`audio.mp3`))
             audio.pipe(res);
             audio.on("data",(data)=>{
                 console.log(data);
@@ -39,7 +42,7 @@ app.post("/api/send/audio", (req, res) => {
                 // res.download(`./audio.mp3`,`${title}.mp3`)
             })
             audio.on("error", (err) => {
-                console.log("Error: " + err);
+                console.log("Error in audio: " + err);
             })
         }
         getAudio();
@@ -52,7 +55,7 @@ app.post("/api/send/video", (req, res) => {
     console.log(url);
     if (url) {
         
-        const getAudio = async () => {
+        const getVideo = async () => {
 
             const response = await axios.get(url)
             const html = response.data
@@ -61,27 +64,27 @@ app.post("/api/send/video", (req, res) => {
             console.log(title, typeof title);
             title=title.slice(0,30);
             console.log(title, typeof title);
-            res.setHeader("Content-Type", "video/mp4");
+            res.setHeader("Content-Type", "audio/mpeg");
             res.setHeader(`Content-Disposition`, `attachment; filename=${title}.mp4`);
-            const audio = ytdl(url, { quality: '18' });
-            // await audio.pipe(fs.createWriteStream(`audio.mp3`))
-            audio.pipe(res);
-            audio.on("data",(data)=>{
+            const video = ytdl(url, { quality: '18' });
+            await video.pipe(fs.createWriteStream(`video.mp3`))
+            video.pipe(res);
+            video.on("data",(data)=>{
                 console.log(data);
             })
-            audio.on("finish", () => {
+            video.on("finish", () => {
                 console.log("Piped!!!!!")
-                // res.download(`./audio.mp3`,`${title}.mp3`)
+                // res.download(`./video.mp3`,`${title}.mp3`)
             })
-            audio.on("error", (err) => {
+            video.on("error", (err) => {
                 console.log("Error: " + err);
             })
         }
-        getAudio();
+        getVideo();
     }
 })
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err) console.log("Error");;
     console.log("The server is up and running.")
 })
